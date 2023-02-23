75a09b8ee49a8b9abbcd34c822b551542eb492ec
bloombar <amos@cs.nyu.edu>
2023-02-22 22:39:35 -0500

    environment variables for all!

--- log_commits.py
+++ log_commits.py
@@ -1,55 +1,77 @@
 #!/usr/bin/env python 
 
+import subprocess
+import re
+import json
+import logging
+import logging.handlers
+import argparse
+
+def setup_logging(logfile):
+  '''
+  Setup logging to a file.
+  @param logfile: The path to the log file.
+  @return: The logger object.
+  '''
+  # Setup logging
+  logger = logging.getLogger('log_commits')
+  logger.setLevel(logging.DEBUG)
+  # create file handler which logs even debug messages
+  fh = logging.handlers.RotatingFileHandler(logfile, maxBytes=1000000, backupCount=5)
+  fh.setLevel(logging.DEBUG)
+  logger.addHandler(fh)    
+  return logger
+
+def get_args():
+  '''
+  Parse command-line arguments.
+  @return: The parsed arguments.
+  '''
+  # parse command-line arguments
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-c", "--commits", help="JSON array of commits from GitHub Action context variable, github.event.commits", default=[], required=True)
+  parser.add_argument("-u", "--url", help="The URL of the web app where the commit stats should be sent.", default='')
+  args = parser.parse_args()
+  return args
+
 def main():
-    import sys
-    import os
-    import subprocess
-    import re
-    import time
-    import datetime
-    import json
-    import logging
-    import logging.handlers
 
-    # Setup logging
-    logger = logging.getLogger('log_commits')
-    logger.setLevel(logging.DEBUG)
-    # create file handler which logs even debug messages
-    fh = logging.handlers.RotatingFileHandler('log_commits.log', maxBytes=1000000, backupCount=5)
-    fh.setLevel(logging.DEBUG)
-    logger.addHandler(fh)    
+  # set up logging
+  logger = setup_logging('data.csv')
 
-    # Get the current directory
-    current_dir = os.getcwd()
+  # set up command line arguments
+  parser = get_args()
 
-    # Get the git log
-    commit_ids = ['482d0d1842a25e3f564aec39c4bf879b95e7fe02', '04c3a74f3b3405fac6bc27368a0a516225139b80', 'eda541d6ba86bcf2c9c1eebb67f208044aafc896']
-    logger.info('commit_id,commit_author_name,commit_author_email,commit_date,commit_message,commit_files,commit_additions,commit_deletions')
-    for commit_id in commit_ids:
-      # get git stats for this commit
-      cmd = f"git show --shortstat {commit_id}" #--date=format-local:'%m/%d/%Y %H:%M:%S'"
-      git_log = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-      git_log_out, git_log_err = git_log.communicate()
-      git_log_out = git_log_out.decode('UTF-8') # convert bytes to string
+  # Get the git log
+  # commit_ids = ['482d0d1842a25e3f564aec39c4bf879b95e7fe02', '04c3a74f3b3405fac6bc27368a0a516225139b80', 'eda541d6ba86bcf2c9c1eebb67f208044aafc896']
+  commit_ids = [commit['id'] for commit in json.loads(parser.commits)] #extract commit ids from the GitHub Action context variable
+  print(commit_ids)
+  logger.info('commit_id,commit_author_name,commit_author_email,commit_date,commit_message,commit_files,commit_additions,commit_deletions')
+  for commit_id in commit_ids:
+    # get git stats for this commit
+    cmd = f"git show --shortstat {commit_id}" #--date=format-local:'%m/%d/%Y %H:%M:%S'"
+    git_log = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    git_log_out, git_log_err = git_log.communicate()
+    git_log_out = git_log_out.decode('UTF-8') # convert bytes to string
 
-      # parse git commit log
-      m = re.match(r"commit ([a-zA-Z0-9]+).*\nAuthor:\s(.*)\s<((.*))>.*\nDate:\s(.*)\n\n(.*)\n\n(.*?(\d+) file[s]? changed)?(.*?(\d+) insertion[s]?)?(.*?(\d+) deletion[s]?)?", git_log_out)
-      if not m is None:
-        # basic commit info
-        commit_id = m.groups(0)[0].strip()
-        commit_author_name = m.groups(0)[1].strip()
-        commit_author_email = m.groups(0)[2].strip()
-        commit_date = m.groups(0)[4].strip()
-        commit_message = m.groups(0)[5].replace('[,"]', '').strip() # remove any quotes and commas to make a valid csv
-        # stats
-        commit_files = m.groups(0)[7].strip()
-        commit_additions = m.groups(0)[9].strip() if len(m.groups(0)) > 9 else 0
-        commit_deletions = str(m.groups(0)[11]).strip() if len(m.groups(0)) > 11 else 0
-        # print(f"{commit_id},{commit_author_name},{commit_author_email},{commit_date},{commit_message},{commit_files},{commit_additions},{commit_deletions}")
-        logger.info(f'{commit_id},{commit_author_name},{commit_author_email},{commit_date},"{commit_message}",{commit_files},{commit_additions},{commit_deletions}')
-      else:
-        # print('no match')
-        pass
+    # parse git commit log
+    m = re.match(r"commit ([a-zA-Z0-9]+).*\nAuthor:\s(.*)\s<((.*))>.*\nDate:\s(.*)\n\n(.*)\n\n(.*?(\d+) file[s]? changed)?(.*?(\d+) insertion[s]?)?(.*?(\d+) deletion[s]?)?", git_log_out)
+    if not m is None:
+      # basic commit info
+      commit_id = m.groups(0)[0].strip()
+      commit_author_name = m.groups(0)[1].strip()
+      commit_author_email = m.groups(0)[2].strip()
+      commit_date = m.groups(0)[4].strip()
+      commit_message = m.groups(0)[5].replace('[,"]', '').strip() # remove any quotes and commas to make a valid csv
+      # stats
+      commit_files = m.groups(0)[7].strip()
+      commit_additions = m.groups(0)[9].strip() if len(m.groups(0)) > 9 else 0
+      commit_deletions = str(m.groups(0)[11]).strip() if len(m.groups(0)) > 11 else 0
+      # print(f"{commit_id},{commit_author_name},{commit_author_email},{commit_date},{commit_message},{commit_files},{commit_additions},{commit_deletions}")
+      logger.info(f'{commit_id},{commit_author_name},{commit_author_email},{commit_date},"{commit_message}",{commit_files},{commit_additions},{commit_deletions}')
+    else:
+      # print('no match')
+      pass
 
 if __name__ == "__main__":
   main()
