263a29fca7c18489b561965d40aef013820cec0d
bloombar <amos@cs.nyu.edu>
2023-02-23 21:03:52 -0500

    shit

--- log_commits.py
+++ log_commits.py
@@ -69,12 +69,13 @@ def get_commit_data(commit_id, exclusions):
   @param exclusions: The files to exclude from the git stats.
   @return: A dictionary containing the git stats for the commit.
   '''
+  print(f'commit_id: {commit_id}')
   date_format = r"--date=format-local:'%m/%d/%Y %H:%M:%S'" # formatted in a way that works well in Google Sheets
   cmd = f"git show --shortstat {date_format} {commit_id} {exclusions}"
   git_log = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   git_log_out, git_log_err = git_log.communicate()
   git_log_out = git_log_out.decode('UTF-8') # convert bytes to string
-  # print(f'git_log_out: {git_log_out}')
+  print(f'git_log_out: {git_log_out}')
 
   # parse git commit log
   commit_data = {} # start off blank
