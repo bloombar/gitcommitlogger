d4641168c2c561aeecac204aca3eea7dd868ea3d
bloombar <amos@cs.nyu.edu>
2023-02-23 19:04:48 -0500

    another try at json

--- log_commits.py
+++ log_commits.py
@@ -124,7 +124,7 @@ def main():
     # convert the list of commits to a JSON string
     commits_json = json.dumps(commits_list)
     # send the data to the web app in a POST request
-    r = requests.post(args.url, json=commits_json)
+    r = requests.post(args.url, json=commits_list)
     print(r.status_code, r.reason, r.content, r.text) #typof
 
 if __name__ == "__main__":
